7516226c645abe4a2134d620565572edc229d1a6
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-03 01:12:47 +0530

    add route for landing and 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <>
-            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+            <Header />
             <Routes>
-                <Route />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
+            <Footer />
         </>
     );
 }
