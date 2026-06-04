898ed46fd7fef6fd6c1b8b1c573ddcb2cb2d022b
Nilotpal04 <nilotpalb2004@gmail.com>
2026-06-04 09:11:50 +0000

    add exception handling to /upload endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,28 +9,35 @@ from backend.app.parsers.ocr_parser import parse_ocr_text
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Backend working"}
 
 @app.post("/upload")
 async def upload_file(file: UploadFile):
-    file_path = f"app/uploads/{file.filename}"
-    
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-        
-    if file.filename.endswith(".pdf"):
-        text = extract_pdf_text(file_path)
-        data = parse_pdf_text(text)
-    else:
-        text = extract_image_text(file_path)
-        data = parse_ocr_text(text)
-        
-    return {
-        "message": "success",
-        "data": data
-    }
+
+    try:
+        file_path = f"app/uploads/{file.filename}"
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        if file.filename.endswith(".pdf"):
+            text = extract_pdf_text(file_path)
+            data = parse_pdf_text(text)
+
+        else:
+            text = extract_image_text(file_path)
+            data = parse_ocr_text(text)
+
+        return {
+            "success": True,
+            "filename": file.filename,
+            "data": data
+        }
+
+    except Exception as e:
+        return {
+            "success": False,
+            "error": str(e)
+        }
     
     
     
